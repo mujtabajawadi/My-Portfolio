59e3f5d7ce261a48625577b5c85a5c9bf80e3bea
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-04-24 19:43:07 +0500

    Hero Section Text Animation Completed

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
+const firstPageAnimation = () => {
+  var tl = gsap.timeline();
 
-const mouseFollowerCircel = () => {
-    document.addEventListener('mousemove', function (details) {
-       document.querySelector('#mini-circle').style.transform = `translate(${details.clientX -4}px, ${details.clientY -4}px)`
-    })
-}
+  tl.from("#nav", {
+    y: "-20",
+    opacity: 0,
+    duration: 1.5,
+    // delay: '-0.5',
+    ease: "slow",
+  }).to(".text-to-animate", {
+    y: 0,
+    ease: "slow",
+    stagger: 0.3,
+    delay: "-1",
+  })
+};
 
-mouseFollowerCircel()
+const mouseFollowerCircle = () => {
+  window.addEventListener("mousemove", function (details) {
+    document.querySelector("#mini-circle").style.transform = `translate(${
+      details.clientX - 4
+    }px, ${details.clientY - 4}px)`;
+  });
+};
+
+mouseFollowerCircle();
+firstPageAnimation();
