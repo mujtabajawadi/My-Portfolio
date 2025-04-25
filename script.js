fe83a7f96cd160639706ab6d08ed97220e6c0b15
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-04-25 19:27:38 +0500

    Mouse Follower Circle Distort Animation Completed

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
+var timeout;
+
 const firstPageAnimation = () => {
   var tl = gsap.timeline();
 
@@ -17,14 +19,41 @@ const firstPageAnimation = () => {
     ease: "slow",
     stagger: 0.3,
     delay: "-1",
-  })
+  });
 };
 
-const mouseFollowerCircle = () => {
+const mouseCircleDistort = () => {
+  var xScale = 1;
+  var yScale = 1;
+
+  var previousX = 0;
+  var previousY = 0;
+  window.addEventListener("mousemove", (details) => {
+
+    clearTimeout(timeout)
+    xScale = gsap.utils.clamp(0.8, 1.2, details.clientX - previousX);
+    yScale = gsap.utils.clamp(0.8, 1.2, details.clientY - previousY);
+
+    previousX = details.clientX;
+    previousY = details.clientY;
+
+    mouseFollowerCircle(xScale, yScale);
+
+    timeout = setTimeout(() => {
+      document.querySelector("#mini-circle").style.transform = `translate(${
+        details.clientX - 6
+      }px, ${details.clientY - 5}px) scale(1, 1)`;
+    },100)
+  });
+};
+
+mouseCircleDistort()
+
+const mouseFollowerCircle = (xScale, yScale) => {
   window.addEventListener("mousemove", function (details) {
     document.querySelector("#mini-circle").style.transform = `translate(${
-      details.clientX - 4
-    }px, ${details.clientY - 4}px)`;
+      details.clientX - 6
+    }px, ${details.clientY - 5}px) scale(${xScale}, ${yScale})`;
   });
 };
 
