30cc0ed56bec0101522567a7f0a625e575ba1134
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-04-26 20:18:07 +0500

    Work section images animation completed but there are some bugs remaining to fix

--- script.js
+++ script.js
@@ -59,3 +59,62 @@ const mouseFollowerCircle = (xScale, yScale) => {
 
 mouseFollowerCircle();
 firstPageAnimation();
+
+
+document.querySelectorAll('.work').forEach((element) => {
+   var rotate = 0;
+  var positionDifference = 0;
+  var isVisible = false
+
+  var image = element.querySelector("img");
+  image.style.display = 'none'
+  
+ 
+  element.addEventListener('mousemove', (details) => {
+
+    var diff = details.clientY - element.getBoundingClientRect().top//mouse position inside div element
+    console.log(diff)
+    positionDifference = details.clientX - rotate
+    rotate = details.clientX
+
+
+   
+
+    const imageWidth = image.offsetWidth;
+    const imageHeight = image.offsetHeight;
+
+    
+    if (!isVisible) {
+      gsap.to(image, {
+        display: 'block',
+        opacity: 1,
+        ease: Power3,
+        top: diff - (imageHeight / 2),
+        left: details.clientX - (imageWidth / 2),
+        rotate: gsap.utils.clamp(-15, 15, positionDifference),
+      });
+      isVisible = true
+    }
+
+      gsap.to(image, {
+    top: diff - (imageHeight/2),
+    left: details.clientX - (imageWidth/2),
+    rotate: gsap.utils.clamp(-15, 15, positionDifference),
+    ease: Power3,
+    duration: 0.2
+  });
+});
+  })
+  
+    element.addEventListener("mouseleave", (details) => {
+      var image = element.querySelector("img");
+        gsap.to(image, {
+          opacity: 0,
+          onComplete: () => {
+            image.style.display = "none";
+            isVisible = false;
+          },
+        });
+      
+    });
+
