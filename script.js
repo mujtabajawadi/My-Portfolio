76cedf9fc32ad2b9175340b9df00c045a6d30f65
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-11-23 10:45:18 +0500

    Revert "Add SVG and PNG thumbnails for various technologies"
    This reverts commit 935b92525571b8f30de767bc284b04afdf8076ff.

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-const footerTime = document.getElementById("time");
-const footerYear = document.getElementById("year")
-var timeout;
-
-
-
-const lenis = new Lenis({
-  duration: 1.8
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
+var timeout;
 
-requestAnimationFrame(raf);
 const firstPageAnimation = () => {
   var tl = gsap.timeline();
 
@@ -22,6 +12,7 @@ const firstPageAnimation = () => {
     y: "-20",
     opacity: 0,
     duration: 1.5,
+    // delay: '-0.5',
     ease: "slow",
   }).to(".text-to-animate", {
     y: 0,
@@ -55,6 +46,8 @@ const mouseCircleDistort = () => {
   });
 };
 
+mouseCircleDistort();
+
 const mouseFollowerCircle = (xScale, yScale) => {
   window.addEventListener("mousemove", function (details) {
     document.querySelector("#mini-circle").style.transform = `translate(${
@@ -63,63 +56,66 @@ const mouseFollowerCircle = (xScale, yScale) => {
   });
 };
 
-const currentTime = () => {
-  const time = new Date();
-  console.log(time.toLocaleTimeString())
-
-  let fullTime = time.toLocaleTimeString()
-
-  footerTime.innerText = `${fullTime}  PST`
-  footerYear.innerText = time.getFullYear() 
-
-  // let hours = time.getHours();
-  // let minutes = time.getMinutes();
-  // if (hours > 12) {
-  //   footerTime.innerText = `${hours - 12}: ${minutes} PM PST`;
-  // } else if (hours == "00") {
-  //   footerTime.innerText = `12 : ${minutes} AM PST`;
-  // } else {
-  //   footerTime.innerText = `${hours} : ${minutes} AM PST`;
-  // }
-};
-
-setInterval(() => {
-  currentTime();
-}, 1000);
-
 mouseFollowerCircle();
 firstPageAnimation();
-mouseCircleDistort();
 
 document.querySelectorAll(".work").forEach((element) => {
   var rotate = 0;
   var positionDifference = 0;
-  var image = element.querySelector("img");
+  var image = element.querySelector('img')
+ 
+  const imageWidth = image.offsetWidth
+  const imageHeight = image.offsetHeight
 
-  const imageWidth = image.offsetWidth;
-  const imageHeight = image.offsetHeight;
 
-  element.addEventListener("mousemove", (details) => {
-    var diff = details.clientY - element.getBoundingClientRect().top; //mouse position inside div element
-    positionDifference = details.clientX - rotate;
-    rotate = details.clientX;
 
-    gsap.to(image, {
-      opacity: 1,
-      zIndex: 9999,
-      ease: Power3,
-      top: diff - imageHeight / 2,
-      left: details.clientX - imageWidth / 2,
-      rotate: gsap.utils.clamp(-15, 15, positionDifference * 0.5),
-    });
-  });
+
+  element.addEventListener('mouseenter', () => {
+    mouseInsideDiv = true
+
+   
+  })
+
+   element.addEventListener("mousemove", (details) => {
+    
+       var diff = details.clientY - element.getBoundingClientRect().top; //mouse position inside div element
+       positionDifference = details.clientX - rotate;
+       rotate = details.clientX;
+
+       gsap.to(image, {
+         opacity: 2,
+         ease: Power1,
+         top: diff - imageHeight / 2,
+         left: details.clientX - imageWidth / 2,
+         rotate: gsap.utils.clamp(-15, 15, positionDifference),
+       });
+     
+   });
+
+
+
+  
 
   element.addEventListener("mouseleave", () => {
-    gsap.to(image, {
-      opacity: 0,
-      zIndex: -1,
+      
+    
+      gsap.to(image, {
+        opacity: 0,
+        zIndex: -1
+      });
+
+
+    element.removeEventListener('mousemove')
     });
-  });
 });
 
 
+const footer = document.querySelectorAll('.footer-link')
+footer.forEach((elem) => {
+  elem.addEventListener('mouseover', () => {
+    elem.style.textDecoration = 'underline'
+  })
+  elem.addEventListener('mouseleave', () => {
+    elem.style.textDecoration = 'none'
+  })
+})
